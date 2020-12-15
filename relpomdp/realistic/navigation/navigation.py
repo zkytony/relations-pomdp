06c23e3169ef2c6f2a86e6d17ff0c8433b5a97da
Kaiyu Zheng <kaiyutony@gmail.com>
2020-12-15 08:42:01 -0500

    Navigation works at small scale

--- relpomdp/realistic/navigation/navigation.py
+++ relpomdp/realistic/navigation/navigation.py
@@ -4,6 +4,10 @@
 import pomdp_py as pdp
 import math
 import random
+from relpomdp.realistic.environment import ThorEnv
+from relpomdp.realistic.utils.ai2thor_utils import save_frames,\
+    plot_reachable_grid, get_reachable_pos_set
+import matplotlib.pyplot as plt
 
 # State
 class NavState(pdp.State):
@@ -30,7 +34,7 @@ class NavState(pdp.State):
         return False
 
     def __hash__(self):
-        return hash(self.pos, self.rot)
+        return hash((self.pos, self.rot))
 
 # Action
 class Action(pdp.Action):
@@ -104,7 +108,8 @@ SIGN = lambda x: -1.0 if x < 0.0 else 1.0
 # Transition model
 class TransitionModel(pdp.TransitionModel):
 
-    def __init__(self, grid_size=0.25):
+    def __init__(self, reachable_positions, grid_size=0.25):
+        self.reachable_positions = reachable_positions
         self.grid_size = grid_size
 
     def sample(self, state, action):
@@ -132,7 +137,10 @@ class TransitionModel(pdp.TransitionModel):
         x = self.grid_size * round((x + dx) / self.grid_size)
         z = self.grid_size * round((z + dz) / self.grid_size)
         rot = rot % 360
-        return NavState((x,z), rot)
+        if (x,z) in self.reachable_positions:
+            return NavState((x,z), rot)
+        else:
+            return NavState(state.pos, state.rot)
 
 
 # Observation
@@ -160,7 +168,7 @@ class NavObservation(pdp.Observation):
         return False
 
     def __hash__(self):
-        return hash(self.pos, self.rot)
+        return hash((self.pos, self.rot))
 
 # Observation model: A noiseless observation model
 class ObservationModel(pdp.ObservationModel):
@@ -181,18 +189,117 @@ class RandomPolicyModel(pdp.RolloutPolicy):
     def rollout(self, state, history=None):
         return self.sample(state)
 
+    def get_all_actions(self, state=None, history=None):
+        return self.actions
+
 # Reward model
 class NavRewardModel(pdp.RewardModel):
-    def __init__(self, goal_pose):
-        pos, rot = goal_pose
-        self.goal_pose = (pos, round(rot, 2) % 360.0)
+    def __init__(self, goal_pos):
+        self.goal_pos = goal_pos
 
     def sample(self, state, action, next_state, **kwargs):
         return self.argmax(state, action, next_state)
 
     def argmax(self, state, action, next_state, **kwargs):
-        next_pose = (next_state.pos, round(next_state.rot, 2) % 360.0)
-        if predicted_pose == expected_pose:
+        if next_state.pos == self.goal_pos:
             return 100
         else:
             return -1
+
+# Agent
+class NavAgent(pdp.Agent):
+    def __init__(self,
+                 init_pose, goal_pose, actions, reachable_positions,
+                 nparticles=100, grid_size=0.25):
+        init_belief = pdp.Particles([NavState(*init_pose) for i in range(nparticles)])
+        super().__init__(init_belief,
+                         RandomPolicyModel(actions),
+                         TransitionModel(reachable_positions, grid_size),
+                         ObservationModel(),
+                         NavRewardModel(goal_pose))
+
+# Test
+def test(scene_name, nparticles=1000, grid_size=0.25, degrees=90):
+    config = {
+        "scene_name": scene_name,
+        "agent_mode": "default",
+        "width": 400,
+        "height": 400
+    }
+    motions = build_motion_actions(grid_size=grid_size, degrees=degrees)
+
+    env = ThorEnv(config)
+    env.launch()
+    reachable_positions = get_reachable_pos_set(env.controller, use_2d=True)
+
+    # plotting
+    plt.ion()
+    fig = plt.figure(figsize=(6,3))
+    ax = fig.add_subplot(1, 1, 1)#, projection="3d")
+    plt.show(block=False)
+
+    # problem instance
+    init_pose = env.agent_pose(use_2d=True)
+    goal_pos = random.sample(reachable_positions, 1)[0]
+    init_y = env.agent_pose()[0][1]  # Note: needed due to Ai2thor bug
+    print("Init pose: {}     Goal pose: {}".format(init_pose, goal_pos))
+
+    agent = NavAgent(init_pose, goal_pos, motions, reachable_positions,
+                     nparticles=100,
+                     grid_size=grid_size)
+    planner = pdp.POMCP(max_depth=100,
+                        discount_factor=0.95,
+                        num_sims=2000,
+                        exploration_const=100,
+                        rollout_policy=agent.policy_model)
+
+    # Environment
+    init_state = NavState(*init_pose)
+    transition_model = agent.transition_model
+    reward_model = agent.reward_model
+    observation_model = agent.observation_model
+    state = init_state
+
+    for step in range(100):
+        action = planner.plan(agent)
+        next_state = transition_model.sample(state, action)
+
+        # Check whether next_state should be applied
+        if next_state.pos not in reachable_positions:
+            # next state pos is invalid
+            next_state = state  # no state transition
+        else:
+            # Yes, robot can move to next state
+            if degrees != 90:
+                print("Warning: rotation degree isn't 90, Ai2thor motion model"\
+                      "doesn't exactly match my transition model.")
+            env.controller.step('TeleportFull',
+                                x=next_state.pos[0], y=init_y, z=next_state.pos[1],
+                                rotation=dict(y=next_state.rot))
+
+        observation = observation_model.sample(next_state, action)
+        reward = reward_model.sample(state, action, next_state)
+
+        plot_reachable_grid(env.controller, ax, agent_pose=env.agent_pose())
+        ax.scatter([goal_pos[0]], [goal_pos[1]], c='g')
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+        # belief update
+        try:
+            planner.update(agent, action, observation)
+        except ValueError:
+            true_pose = env.agent_pose(use_2d=True)
+            agent.set_belief(pdp.Particles([NavState(*true_pose)
+                                            for i in range(nparticles)]))
+        print("Step {} | Reward: {} | Action: {} | Observation: {} | Belief MPE: {}"\
+              .format(step, reward, action, observation, agent.belief.mpe()))
+
+        state = next_state
+        if state.pos == goal_pos:
+            print("Done.")
+            break
+
+
+if __name__ == "__main__":
+    test("FloorPlan18", nparticles=1000)
