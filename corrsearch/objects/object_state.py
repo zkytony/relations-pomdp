45ea1cfa0d400326c4906b161e88d20b14374ae4
Kaiyu Zheng <kaiyutony@gmail.com>
2021-02-03 10:43:45 -0500

    object states

---
+++ corrsearch/objects/object_state.py
@@ -0,0 +1,160 @@
+"""Objects state
+
+An object can be literally represented just by a number, its ID.
+
+What is more meaningful is the state of the object, ObjectState.
+
+This has been defined in the OOPOMDP framework, but I will
+port over the ObjectState class here, specifically.
+
+It is, perhaps, the same framework as OOPOMDP. So, probably I
+should reuse the code there. That is the right thing to do.
+"""
+from pomdp_py.framework.basics import POMDP, State, Action, Observation,\
+    ObservationModel, TransitionModel, GenerativeDistribution, Environment, Agent, RewardModel
+import pprint
+import copy
+
+
+class ObjectState(State):
+    # Note: 08/22/2020 - it's a copy of the ObjectState from pomdp_py
+    """
+    This is the result of OOState factoring; A state
+    in an OO-POMDP is made up of ObjectState(s), each with
+    an `object class` (str) and a set of `attributes` (dict).
+    """
+    def __init__(self, objid, objclass, attributes):
+        """
+        Args:
+            objid (int): ID of the object
+            class: "class",
+            attributes: Dictionary, mapping from attribute name
+                        to a HASHABLE attribute.
+                {
+                    "attr1": Attribute,
+                    ...
+                }.
+
+        """
+        self.id = objid
+        self.objclass = objclass
+        self.attributes = attributes
+        self._hashcode = hash(frozenset(self.attributes.items()))
+
+    def __repr__(self):
+        return '{}::({},{},{})'.format(self.__class__.__name__,
+                                       self.objid,
+                                       self.objclass,
+                                       self.attributes)
+
+    def __str__(self):
+        s = "{}#{} [{}]\n".format(self.objclass,
+                                  self.objid,
+                                  self.__class__.__name__)
+        s += pprint.pformat(self.attributes, indent=2)
+        return s
+
+    def __hash__(self):
+        return self._hashcode
+
+    def __eq__(self, other):
+        return self.objclass == other.objclass\
+            and self.attributes == other.attributes
+
+    @property
+    def objid(self):
+        return self.id
+
+    def __getitem__(self, attr):
+        """__getitem__(self, attr)
+        Returns the attribute"""
+        return self.attributes[attr]
+
+    def __setitem__(self, attr, value):
+        """__setitem__(self, attr, value)
+        Sets the attribute `attr` to the given value."""
+        raise NotImplementedError("ObjectState is immutable.")
+
+    def __len__(self):
+        return len(self.attributes)
+
+    def copy(self):
+        """copy(self)
+        Copies the state.
+        You should override this method for efficiency,
+        if deepcopy is not necessary"""
+        return copy.deepcopy(self)
+
+    def get(self, attr, default_val):
+        if attr in self.attributes:
+            return self.attributes[attr]
+        else:
+            return default_val
+
+
+class OOState(State):
+    """
+    State that can be factored by objects, that is, to ObjectState(s).
+
+    __init__(self, object_states)
+    """
+
+    def __init__(self, object_states):
+        """
+        Args:
+            objects_states (dict, or array-like): dictionary {ID:ObjectState},
+                or an array like object consisting of ObjectStates
+        """
+        if type(object_states) != dict:
+            # expect object_states to be enumerable as an array
+            object_states_dict = {sobj.id : sobj
+                                  for sobj in object_states}
+            # Check id uniqueness
+            assert len(object_states_dict) == len(object_states),\
+                "object_states contains duplicated id"
+            object_states = object_states_dict
+        else:
+            # Check integrity of object id in dict
+            for objid in object_states:
+                assert object_states[objid].id == objid,\
+                    "Object state id mismatch (%d != %d)" % (objid, object_states[objid].id)
+
+        self.object_states = object_states
+        self._situation = frozenset(self.object_states.items())
+        self._hashcode = hash(self._situation)
+
+    def __str__(self):
+        return pprint.pformat(self.object_states,
+                              indent=2)
+
+    def __repr__(self):
+        return '%s::[%s]' % (str(self.__class__.__name__),
+                             repr(self.object_states))
+
+    def __eq__(self, other):
+        return isinstance(other, OOState)\
+            and self.object_states == other.object_states
+
+    def __hash__(self):
+        return self._hashcode
+
+    def obj(self, objid):
+        return self.get_object_state(objid)
+
+    def __getitem__(self, objid):
+        """__getitem__(self, attr)
+        Returns the attribute"""
+        return self.object_states[objid]
+
+    def __setitem__(self, attr, value):
+        """__setitem__(self, attr, value)
+        Sets the attribute `attr` to the given value."""
+        raise NotImplementedError("ObjectState is immutable.")
+
+    def copy(self):
+        object_states = {objid : self.object_states[objid].copy()
+                         for objid in self.object_states}
+        return OOState(object_states)
+
+    def __len__(self):
+        return len(self.object_states)
---
+++ corrsearch/objects/tests.py
@@ -0,0 +1,53 @@
+from corrsearch.objects import ObjectState, OOState
+import unittest
+
+class TestObjectStateOOState(unittest.TestCase):
+    def setUp(self):
+        self.so1 = ObjectState(1, "cup",
+                               {"location": (5,3),
+                                "temperature": 70})
+        self.so2 = ObjectState(2, "juice",
+                               {"location": (5,5),
+                                "kind": "apple"})
+        self.so3 = ObjectState(3, "bag",
+                               {"location": (5,10),
+                                "contains": (1, 2),
+                                "owner": ("Bob", 40, "worker")})
+        self.s = OOState({self.so1, self.so2, self.so3})
+
+    def test_hashable_attributes_only(self):
+        with self.assertRaises(TypeError):
+            so3 = ObjectState(3, "bag",
+                              {"items": {1, 2}})
+        # OK if it's tuple
+        so3 = ObjectState(3, "bag",
+                          {"items": (1, 2)})
+
+    def test_objid_checking(self):
+        so4 = ObjectState(1, "cup", {})
+        with self.assertRaises(AssertionError):
+            OOState({self.so1, self.so2, self.so3, so4})
+        so4 = ObjectState(4, "cup", {})
+        with self.assertRaises(AssertionError):
+            OOState({self.so1.id:self.so1,
+                     self.so1.id: so4})
+
+    def test_copy_equal(self):
+        self.assertEqual(self.so1, self.so1.copy())
+        self.assertEqual(self.so2, self.so2.copy())
+        self.assertEqual(self.s, self.s.copy())
+
+    def test_immutable(self):
+        with self.assertRaises(NotImplementedError):
+            self.so1["location"] = (5,5)
+        with self.assertRaises(NotImplementedError):
+            self.s[self.so1.id] = self.so2
+
+
+    def test_hashable(self):
+        hash(self.so1)
+        hash(self.so2)
+
+
+if __name__ == "__main__":
+    unittest.main()
