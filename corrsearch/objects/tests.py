bf4550b479fb2e27302727c3e6941d25a054c064
Kaiyu Zheng <kaiyutony@gmail.com>
2021-02-03 11:26:20 -0500

    minor test

--- corrsearch/objects/tests.py
+++ corrsearch/objects/tests.py
@@ -1,4 +1,5 @@
 from corrsearch.objects import ObjectState, JointState, ObjectObz, JointObz
+import pomdp_py
 import unittest
 
 class TestObjectState(unittest.TestCase):
@@ -14,6 +15,9 @@ class TestObjectState(unittest.TestCase):
                                 "contains": (1, 2),
                                 "owner": ("Bob", 40, "worker")})
         self.s = JointState({self.so1, self.so2, self.so3})
+
+    def test_inheritence(self):
+        self.assertIsInstance(self.so1, pomdp_py.State)
 
     def test_hashable_attributes_only(self):
         with self.assertRaises(TypeError):
@@ -51,50 +55,53 @@ class TestObjectState(unittest.TestCase):
 
 class TestObjectObz(unittest.TestCase):
     def setUp(self):
-        self.so1 = ObjectObz(1, "cup",
+        self.zo1 = ObjectObz(1, "cup",
                              {"location": (5,3),
                               "temperature": 70})
-        self.so2 = ObjectObz(2, "juice",
+        self.zo2 = ObjectObz(2, "juice",
                              {"location": (5,5),
                               "kind": "apple"})
-        self.so3 = ObjectObz(3, "bag",
+        self.zo3 = ObjectObz(3, "bag",
                              {"location": (5,10),
                               "contains": (1, 2),
                               "owner": ("Bob", 40, "worker")})
-        self.s = JointObz({self.so1, self.so2, self.so3})
+        self.z = JointObz({self.zo1, self.zo2, self.zo3})
+
+    def test_inheritence(self):
+        self.assertIsInstance(self.zo1, pomdp_py.Observation)
 
     def test_hashable_attributes_only(self):
         with self.assertRaises(TypeError):
-            so3 = ObjectObz(3, "bag",
+            zo3 = ObjectObz(3, "bag",
                               {"items": {1, 2}})
         # OK if it's tuple
-        so3 = ObjectObz(3, "bag",
+        zo3 = ObjectObz(3, "bag",
                           {"items": (1, 2)})
 
     def test_objid_checking(self):
-        so4 = ObjectObz(1, "cup", {})
+        z4 = ObjectObz(1, "cup", {})
         with self.assertRaises(AssertionError):
-            JointObz({self.so1, self.so2, self.so3, so4})
-        so4 = ObjectObz(4, "cup", {})
+            JointObz({self.zo1, self.zo2, self.zo3, z4})
+        z4 = ObjectObz(4, "cup", {})
         with self.assertRaises(AssertionError):
-            JointObz({self.so1.id:self.so1,
-                      self.so1.id: so4})
+            JointObz({self.zo1.id:self.zo1,
+                      self.zo1.id: z4})
 
     def test_copy_equal(self):
-        self.assertEqual(self.so1, self.so1.copy())
-        self.assertEqual(self.so2, self.so2.copy())
-        self.assertEqual(self.s, self.s.copy())
+        self.assertEqual(self.zo1, self.zo1.copy())
+        self.assertEqual(self.zo2, self.zo2.copy())
+        self.assertEqual(self.z, self.z.copy())
 
     def test_immutable(self):
         with self.assertRaises(NotImplementedError):
-            self.so1["location"] = (5,5)
+            self.zo1["location"] = (5,5)
         with self.assertRaises(NotImplementedError):
-            self.s[self.so1.id] = self.so2
+            self.z[self.zo1.id] = self.zo2
 
 
     def test_hashable(self):
-        hash(self.so1)
-        hash(self.so2)
+        hash(self.zo1)
+        hash(self.zo2)
 
 
 if __name__ == "__main__":
