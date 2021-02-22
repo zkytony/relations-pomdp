cd4d07119742ceebc43e9ce9a85c6e6a4c2520c1
Kaiyu Zheng <kaiyutony@gmail.com>
2021-02-22 16:05:38 -0500

    refactoring; no need for conversion.py

--- corrsearch/experiments/domains/thor/thor.py
+++ corrsearch/experiments/domains/thor/thor.py
@@ -3,6 +3,8 @@ Functions related to THOR simulation
 """
 import numpy as np
 from ai2thor.controller import Controller
+from corrsearch.experiments.domains.thor.grid_map import GridMap
+from corrsearch.utils import remap
 
 def reachable_thor_loc2d(controller):
     """
@@ -71,3 +73,46 @@ def thor_apply_pose(controller, pose):
     controller.step("TeleportFull",
                     x=x, y=pos["y"], z=z,
                     rotation=dict(y=th))
+
+
+def convert_scene_to_grid_map(controller, scene_name, grid_size):
+    """Converts an Ai2Thor scene to a GridMap"""
+    x, z = reachable_thor_loc2d(controller)
+
+    # obtain grid indices for coordinates  (origin NOT at (0,0))
+    thor_gx = (x // grid_size).astype(int)
+    thor_gy = (z // grid_size).astype(int)
+    width = max(thor_gx) - min(thor_gx)
+    length = max(thor_gy) - min(thor_gy)
+
+    # save these for later use
+    thor_gx_range = (min(thor_gx), max(thor_gx))
+    thor_gy_range = (min(thor_gy), max(thor_gy))
+
+    # remap coordinates to be nonnegative (origin AT (0,0))
+    gx = remap(thor_gx, min(thor_gx), max(thor_gx), 0, width).astype(int)
+    gy = remap(thor_gy, min(thor_gy), max(thor_gy), 0, length).astype(int)
+
+    # Little test: can convert back
+    try:
+        assert all(remap(gx, min(gx), max(gx), thor_gx_range[0], thor_gx_range[1]).astype(int) == thor_gx)
+        assert all(remap(gy, min(gy), max(gy), thor_gy_range[0], thor_gy_range[1]).astype(int) == thor_gy)
+    except AssertionError as ex:
+        print("Unable to remap coordinates")
+        raise ex
+
+    # grid map positions
+    positions = set(zip(gx, gy))
+
+    # grid map dimensions
+    # obstacles: locations that do not fall into valid positions
+    obstacles = {(x,y)
+                 for x in gx
+                 for y in gy
+                 if (x,y) not in positions}
+
+    grid_map = GridMap(width, length, obstacles,
+                       name=scene_name,
+                       ranges_in_thor=(thor_gx_range, thor_gy_range))
+
+    return grid_map
