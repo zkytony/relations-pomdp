69c41bb3c0bc6da039ed2c4b44dd6b6c7b0cfda6
Kaiyu Zheng <kaiyutony@gmail.com>
2021-02-23 07:47:31 -0500

    Minor - non-effecting adjustment of observation/transition order in trial

--- corrsearch/experiments/trial.py
+++ corrsearch/experiments/trial.py
@@ -70,12 +70,14 @@ class SearchTrial(Trial):
         for step in range(max_steps):
             if self.config["planner"] == "PlaybackPlanner":
                 action, observation = planner.plan(agent, **self.config["planner_exec_config"])
+                reward = env.state_transition(action, execute=True)
             else:
                 action = planner.plan(agent, **self.config["planner_exec_config"])
+                reward = env.state_transition(action, execute=True)
                 observation = env.provide_observation(agent.observation_model, action)
                 if self.config["exec_config"].get("debugging", False):
                     agent.tree.print_children_value()
-            reward = env.state_transition(action, execute=True)
+
             agent.set_belief(agent.belief.update(agent, observation, action))
             planner.update(agent, action, observation)
 
